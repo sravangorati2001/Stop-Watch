80a87b396db19c3316e4c99ab940921ab8a54142
Sravan Kumar <66896592+sravangorati2001@users.noreply.github.com>
2021-06-18 19:40:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,78 +1,44 @@
 
-let time=60;
-let score=0;
-let isPlaying;
+const start=document.querySelector('#start');
+const stop=document.querySelector('#stop');
+const reset=document.querySelector('#reset');
 
-
-const wordInput=document.querySelector('#input');
-const qouteWord=document.querySelector('#current-word');
-const scoreDisplay=document.querySelector('#score');
-const timeDisplay=document.querySelector('#time');
-const button=document.querySelector('#start');
-const alert=document.querySelector('#alert');
-button.addEventListener('click',init);
-
-const words=['hat','the','great' ,'Maratha', 'queen', 'Lokmata' ,'punyashloka', 'Devi',
-'Ahilya',  'sunday','monday','tuesday','wednesday',
-'Holkar', 'river',"Blue", "Red","Orange", "Yellow",
-'lucky','sravan','america','japan','italy','jordan','turkey','statue','generate',
-'stubborn','cocktail','runaway','joke','developer','establishment',
-'hero','javascript','nutrition','desert','deserve','design','designer',
-'desire','desk','desperate','despite','destroy','destruction',
-'revolver','echo','siblings',
-'enemy','energy','enforcement','engage','engine','engineer',
-'investigate','horrendous','symptom','across','act','cabinet',
-'cable','cake','calculate','call','camera',
-'action','active','laughter','magic','master','space','definition'];
-
-timeDisplay.innerHTML=time;
-
-function init(){
- time=60;
- alert.className="collapse" ;
- generateWord(words);
- wordInput.value="";
- wordInput.addEventListener('input',checkWord);
- setTimeout(function repeat() {
-  countDown();
-  setTimeout(repeat, 1000);
-}, 1000);
- setInterval(chackstatus,50);
+const sec=document.querySelector('#seconds');
+const min=document.querySelector('#minutes');
+const msec=document.querySelector('#m-seconds');
+let count=0;
+let flag=false;
+let key;
+function incrementCount(){
+ count++;
+ let minutes =  Math.floor((count/100)/60);
+  let  seconds = Math.floor((count/100) - (minutes*60));
+   let milliSeconds = Math.floor(count -(seconds*100) - (minutes*6000));
+   sec.innerHTML=appendZero(seconds);
+   min.innerHTML=appendZero(minutes);
+   msec.innerHTML=appendZero(milliSeconds);
 }
- function generateWord(words){
-     const randomInd=Math.floor(Math.random() * words.length);
-     qouteWord.innerHTML=words[randomInd];
- }
- function countDown(){
-   if(time>0) time--;
-   else if(time===0)
-   isPlaying=false;
-   timeDisplay.innerHTML=time;
- }
- function chackstatus(){
-   if( !isPlaying && time===0){
-   alert.className="alert alert-warning w-25  mx-3" 
-   wordInput.innerHTML="";
-   score=0;
-   }
- }
- function checkWord(){
-     if(matchWords() && time>0){
-     score++;
-     wordInput.value="";
-     generateWord(words);
-     isPlaying=true;
+ start.addEventListener('click',function(){
+     if(!flag){
+  key= setInterval(incrementCount,10);
+   flag=true;
      }
-     scoreDisplay.innerHTML=score; 
- }
- 
- function matchWords(){
-     if(qouteWord.innerHTML===wordInput.value){
-       
-       return true;
-     }
-     else{
-         verdict.innerHTML="";
-         return false;
-     }
+ });
+stop.addEventListener('click',function(){
+  clearInterval(key);
+});
+reset.addEventListener('click',function(){
+   count=0;
+   minutes=0;
+   seconds=0;
+   milliSeconds=0;
+   sec.innerHTML=appendZero(seconds);
+   min.innerHTML=appendZero(minutes);
+   msec.innerHTML=appendZero(milliSeconds);
+});
+ let appendZero=(num)=>{
+     if(num<=9) 
+     return "0"+num;
+     else 
+     return num;
  }
